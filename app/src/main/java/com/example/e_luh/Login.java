5af1b40eaf6dd9b40b5d1dddb646d35dd1898f55
Jean Rebandaban <jeanrebandaban@gmail.com>
2023-04-28 00:20:46 +0800

    fields validation

--- app/src/main/java/com/example/e_luh/Login.java
+++ app/src/main/java/com/example/e_luh/Login.java
@@ -6,15 +6,50 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
+    Button bProceed, bCancel;
+    EditText etUsername, etPassword;
+    boolean isAllFieldsChecked = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setupActivityLink();
+
+        bProceed = findViewById(R.id.btnlogin);
+        bCancel = findViewById(R.id.btnCancel);
+        etUsername = findViewById(R.id.userName3);
+        etPassword = findViewById(R.id.password4);
+
+        bProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // store the returned value of the dedicated function which checks
+                // whether the entered data is valid or if any fields are left blank.
+                isAllFieldsChecked = CheckAllFields();
+
+                // the boolean variable turns to be true then
+                // only the user must be proceed to the activity2
+                if (isAllFieldsChecked) {
+                    Intent i = new Intent(Login.this, AboutUs.class);
+                    startActivity(i);
+                }
+            }
+        });
+
+        bCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login.this.finish();
+                System.exit(0);
+            }
+        });
     }
 
     private void setupActivityLink() {
@@ -27,5 +62,21 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private boolean CheckAllFields() {
+
+        if (etUsername.length() == 0) {
+            etUsername.setError("This field is required");
+            return false;
+        }
+
+        if (etPassword.length() == 0) {
+            etPassword.setError("This field is required");
+            return false;
+        }
+
+        // after all validation return true.
+        return true;
     }
 }
--- app/src/main/java/com/example/e_luh/Registration.java
+++ app/src/main/java/com/example/e_luh/Registration.java
@@ -1,21 +1,52 @@
 package com.example.e_luh;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Registration extends AppCompatActivity {
 
+    Button bProceed;
+    EditText etUsername, etPassword, etPassword2, etEmail;
+    boolean isAllFieldsChecked = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         setupActivityLink();
+
+        bProceed = findViewById(R.id.btnRegister);
+        etUsername = findViewById(R.id.userName);
+        etPassword = findViewById(R.id.password);
+        etPassword2 = findViewById(R.id.password2);
+        etEmail = findViewById(R.id.email);
+
+        bProceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // store the returned value of the dedicated function which checks
+                // whether the entered data is valid or if any fields are left blank.
+                isAllFieldsChecked = CheckAllFields();
+
+                // the boolean variable turns to be true then
+                // only the user must be proceed to the activity2
+                if (isAllFieldsChecked) {
+                    Intent i = new Intent(Registration.this, Login.class);
+                    startActivity(i);
+                }
+            }
+        });
     }
 
     private void setupActivityLink() {
@@ -28,5 +59,40 @@ public class Registration extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private boolean CheckAllFields() {
+
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        String password2 = etPassword2.getText().toString();
+        String email = etEmail.getText().toString();
+
+
+        if (username.length() == 0) {
+            etUsername.setError("This field is required");
+            return false;
+        }
+
+        if (password.length() == 0) {
+            etPassword.setError("This field is required");
+            return false;
+        }
+
+        if (!password2.equals(password)) {
+            etPassword2.setError("Passwords do not match");
+            return false;
+        }
+
+        if (email.length() == 0 || !isEmailValid(email)) {
+            etEmail.setError("Invalid email format");
+            return false;
+        }
+
+        // after all validation return true.
+        return true;
+    }
+    boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
