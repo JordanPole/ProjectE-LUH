e30262d57494aef3580e8dc04f48ee1d062ac96c
E-LUH <santiagoryanpaul22@gmail.com>
2023-04-28 20:18:49 +0800

    commit

--- app/src/main/java/com/example/e_luh/Security.java
+++ app/src/main/java/com/example/e_luh/Security.java
@@ -1,4 +1,28 @@
 package com.example.e_luh;
 
-public class Security {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Security extends AppCompatActivity {
+
+    public Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security);
+
+        button = findViewById(R.id.nxt);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Security.this,AboutUs.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
