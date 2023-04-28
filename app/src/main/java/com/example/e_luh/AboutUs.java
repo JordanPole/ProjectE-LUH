0cdd89e6b219c43f82b4afb9cb850bcbca07c904
E-LUH <santiagoryanpaul22@gmail.com>
2023-04-28 09:24:14 +0800

    commit

--- app/src/main/java/com/example/e_luh/AboutUs.java
+++ app/src/main/java/com/example/e_luh/AboutUs.java
@@ -3,6 +3,7 @@ package com.example.e_luh;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
@@ -15,8 +16,12 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutus);
-        TextView text = findViewById(R.id. human);
-        text.setMovementMethod(LinkMovementMethod.getInstance());
-    }
 
+        setupHyperLink();
+    }
+    private void setupHyperLink() {
+        TextView t2 = (TextView) findViewById(R.id.human);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
+        t2.setText(Html.fromHtml(getString(R.string.hmn)));
+    }
 }
